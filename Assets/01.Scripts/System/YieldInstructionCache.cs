b34c5a779610d537a3b100d35db073a92a4d2a00
전세훈 <twilivez@gmail.com>
2020-04-18 18:12:05 +0900

    [ADD] YieldInstruction 들을 캐싱 해둘 static class 추가

---
+++ Assets/01.Scripts/System/YieldInstructionCache.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class YieldInstructionCache
+{
+    private static Dictionary<float, WaitForSeconds> waitingSeconds = new Dictionary<float, WaitForSeconds>();
+    private static Dictionary<float, WaitForSecondsRealtime> waitingRealTime = new Dictionary<float, WaitForSecondsRealtime>();
+    private static object waitFrame = null;
+
+    public static object WaitFrame => waitFrame;
+
+    public static WaitForSeconds WaitingSeconds(float waitingTime){
+        if(!waitingSeconds.ContainsKey(waitingTime)){
+            waitingSeconds.Add(waitingTime, new WaitForSeconds(waitingTime));
+        }
+
+        return waitingSeconds[waitingTime];
+    }
+
+    public static WaitForSecondsRealtime WaitingRealTime(float waitingTime){
+        if(!waitingRealTime.ContainsKey(waitingTime)){
+            waitingRealTime.Add(waitingTime, new WaitForSecondsRealtime(waitingTime));
+        }
+
+        return waitingRealTime[waitingTime];
+    }
+}
