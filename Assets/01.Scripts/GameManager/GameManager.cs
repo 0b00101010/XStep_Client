2684d51d005f83703b5a48e885d26c84a59c4819
전세훈 <twilivez@gmail.com>
2020-04-18 17:36:44 +0900

    [ADD] GameManager에 TouchManager 기능 추가
    00. Touch
    01. Swipe
    02. Holding
    
    의 정보를 반환합니다.

--- Assets/01.Scripts/GameManager/GameManager.cs
+++ Assets/01.Scripts/GameManager/GameManager.cs
@@ -6,11 +6,15 @@ public class GameManager : MonoBehaviour
 {
     public static GameManager instance;
 
+    [HideInInspector]
+    public TouchManager touchManager;
+
     private void Awake(){ 
         if(instance is null){
             instance = this;
         }
-
+        
+        touchManager = gameObject.GetComponent<TouchManager>();
         DontDestroyOnLoad(this);
     }
 }
--- Assets/01.Scripts/GameManager/TouchManager.cs
+++ Assets/01.Scripts/GameManager/TouchManager.cs
@@ -4,6 +4,64 @@ using UnityEngine;
 
 public class TouchManager : MonoBehaviour
 {
+    private bool isTouch;
+    private bool isSwipe;
+    private bool isHolding;
+
+    private float minSwipeDistance;
+
+    private Vector2 touchDownNotScreenPosition;
     private Vector2 touchDownPosition;
+    private Vector2 touchHoldingPosition;
     private Vector2 touchUpPosition;
+
+    private Vector2 swipeDirection;
+
+    public Vector2 TouchDownPosition => touchDownPosition;
+    public Vector2 TouchUpPosition => touchUpPosition;
+    public Vector2 TouchHoldingPosition => touchHoldingPosition;
+    public Vector2 SwipeDirection => swipeDirection;
+
+    public bool IsTouch => isTouch;
+    public bool IsSwipe => isSwipe;
+    public bool IsHolding => isHolding;
+
+    private void Awake(){
+        minSwipeDistance = Screen.width / 2;
+    }
+
+    private void Update(){
+        ProcessTouch();
+    }
+
+    private void ProcessTouch(){
+        if(Input.touchCount > 0){
+            Touch tempTouch = Input.touches[0];
+
+            if(tempTouch.phase.Equals(TouchPhase.Began)){
+                isTouch = true;
+                touchDownNotScreenPosition = tempTouch.position;
+                touchDownPosition = Camera.main.ScreenToWorldPoint(touchDownNotScreenPosition);
+            }
+            else if(tempTouch.phase.Equals(TouchPhase.Moved)){
+                Vector2 currentPosition = tempTouch.position;
+                if((currentPosition - touchDownNotScreenPosition).magnitude > minSwipeDistance){
+                    swipeDirection = (currentPosition - touchDownNotScreenPosition).normalized;
+                    isSwipe = true;
+                }
+            }
+            else if (tempTouch.phase.Equals(TouchPhase.Stationary)){
+                isHolding = true;
+                isSwipe = false;
+                touchHoldingPosition = Camera.main.ScreenToWorldPoint(tempTouch.position);
+            }
+            else if(tempTouch.phase.Equals(TouchPhase.Ended)){
+                isTouch = false;
+                isSwipe = false;
+                isHolding = false;
+                touchUpPosition = Camera.main.ScreenToWorldPoint(tempTouch.position);
+            }
+        }
+    }
+
 }
