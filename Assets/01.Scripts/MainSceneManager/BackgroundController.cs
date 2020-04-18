8295e084a459f09f26a840553bb17e18fcfbf143
전세훈 <twilivez@gmail.com>
2020-04-18 19:33:36 +0900

    [ADD] Main Scene의 Background Controller 추가

---
+++ Assets/01.Scripts/MainSceneManager/BackgroundController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BackgroundController : MonoBehaviour
+{
+    [SerializeField]
+    private Image backgroundImage;
+
+    private void Awake(){
+        StartCoroutine(BackgroundRotate());
+    }
+    
+    private IEnumerator BackgroundRotate(){
+        while(true){
+            backgroundImage.gameObject.transform.Rotate(Vector3.forward);
+            yield return YieldInstructionCache.WaitFrame;
+        }
+    }
+}
---
+++ Assets/01.Scripts/MainSceneManager/BackgroundPanner.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundPanner : MonoBehaviour
+{
+    [SerializeField]
+    private Vector2 direction;
+    private Vector2 moveVector;
+
+    private Vector2 limitPosition;
+    private Vector2 initialPosition;
+
+
+    [SerializeField]
+    private GameObject[] panner;
+
+    private void Awake(){
+        limitPosition = Vector2.zero;
+        initialPosition = Vector2.zero;
+
+        limitPosition.x = 9.5f * direction.x;
+        initialPosition.x = -9.5f * direction.x;
+
+        initialPosition.y = gameObject.transform.position.y;
+
+        moveVector = direction / 20;
+        StartCoroutine(MovePanner());
+    }
+
+    private IEnumerator MovePanner(){
+        while(true){
+            for(int i = 0; i < panner.Length; i++){
+                panner[i].transform.Translate(moveVector);
+                LimitCheck(panner[i]);    
+            }
+            yield return YieldInstructionCache.WaitFrame;
+        }
+    }
+
+
+    // FIXME : 비효율적
+    private void LimitCheck(GameObject pannerObject){
+        if(direction.x < 0){
+            if(pannerObject.transform.position.x < limitPosition.x){
+                pannerObject.transform.position = initialPosition;
+            }
+        }  
+
+        else if (direction.x > 0){
+            if(pannerObject.transform.position.x > limitPosition.x){
+                pannerObject.transform.position = initialPosition;
+            }
+        }
+        
+    }
+}
