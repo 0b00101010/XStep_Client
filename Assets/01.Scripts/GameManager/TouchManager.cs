34e0eeb968ab7071abb0ded605a68e4d9b426bd6
전세훈 <twilivez@gmail.com>
2020-04-18 16:52:25 +0900

    [UPDATE] 파일위치 변경

---
+++ Assets/01.Scripts/GameManager/TouchManager.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchManager : MonoBehaviour
+{
+    private Vector2 touchDownPosition;
+    private Vector2 touchUpPosition;
+}
