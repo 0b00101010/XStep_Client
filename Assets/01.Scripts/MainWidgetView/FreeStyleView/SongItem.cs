161c06667dec76976d9f667141ba773b3a158aac
Komorio <twilivez@gmail.com>
2020-04-20 18:15:43 +0900

    [ADD] Song Node가 갖고 있을 Script 추가
    현재는 SongItemInformation을 받아와서 Node를 초기화해주는 역할만 수행합니다.

---
+++ Assets/01.Scripts/MainWidgetView/FreeStyleView/SongItem.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI; 
+
+[ExecuteInEditMode]
+public class SongItem : MonoBehaviour
+{
+
+    [SerializeField]
+    private SongItemInformation songItemInformation;
+
+    [SerializeField]
+    private Image eyeCatchImage;
+
+    [SerializeField]
+    private Text songName;
+
+    [SerializeField]
+    private Text composerName;
+
+    [SerializeField]
+    private GameObject[] stepTags;
+
+    [SerializeField]
+    private Text[] difficultyTexts;
+    
+    private void Awake(){
+
+        if(songItemInformation is null)
+            return;
+            
+        eyeCatchImage.sprite = songItemInformation.EyeCatch;
+        songName.text = songItemInformation.SongName;
+        composerName.text = songItemInformation.ComposerName;
+
+        for(int i = 0; i < songItemInformation.StepTags.Length; i++){
+            stepTags[i].SetActive(true);
+        }
+
+        for(int i = 0; i < songItemInformation.Difficultys.Length; i++){
+            difficultyTexts[i].text = songItemInformation.Difficultys[i].ToString();
+        }
+
+    }
+}
