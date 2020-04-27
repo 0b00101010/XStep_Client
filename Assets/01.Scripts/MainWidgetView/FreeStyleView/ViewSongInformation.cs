404b9844a5dff9f93f4ee697e5669958bcc2fc1e
Komorio <twilivez@gmail.com>
2020-04-27 20:51:46 +0900

    [ADD] SongInformation 창 Viewer 추가

---
+++ Assets/01.Scripts/MainWidgetView/FreeStyleView/ViewSongInformation.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ViewSongInformation : MonoBehaviour
+{
+    [SerializeField]
+    private Image backgroundImage;
+    
+    [SerializeField]
+    private Image[] childWidgetsImage;
+
+    [SerializeField]
+    private Text[] childWidgetsText;
+
+    private List<object> childWidgets = new List<object>();
+
+    private void Awake(){
+        childWidgets.AddRange(childWidgetsImage);
+        childWidgets.AddRange(childWidgetsText);
+    }
+    public void OpenSongInformation(){
+        GameManager.instance.widgetViewer.WidgetsOpen(backgroundImage, childWidgets.ToArray());
+    }
+
+    public void CloseSongInformation(){
+        GameManager.instance.widgetViewer.WidgetsClose(backgroundImage, childWidgets.ToArray());
+    }
+
+}
