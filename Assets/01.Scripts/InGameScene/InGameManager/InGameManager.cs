ce78ef619c339664cd0ef44637a7631921950076
Komorio <twilivez@gmail.com>
2020-05-06 07:04:40 +0900

    [ADD] InGameManager 추가
    현재는 Background Color를 바꾸는 역할만 수행합니다.

---
+++ Assets/01.Scripts/InGameScene/InGameManager/InGameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameManager : MonoBehaviour
+{
+    
+    [SerializeField]
+    private VOIDEvent backgroundColorChangeEvent;
+    
+    private void Start(){
+        StartCoroutine(BackgroundChangeCoroutine());
+    }
+
+    private IEnumerator BackgroundChangeCoroutine(){
+        while(true){
+            backgroundColorChangeEvent.Invoke();
+            yield return YieldInstructionCache.WaitingSeconds(5.0f);
+        }
+    }
+
+}
