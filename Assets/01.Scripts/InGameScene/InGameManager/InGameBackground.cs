80ead46c3f02b334af23db50dcaac8a2a5e3badd
Komorio <twilivez@gmail.com>
2020-05-06 04:02:53 +0900

    [UPDATE] 필요하지 않은 코드 삭제

--- Assets/01.Scripts/InGameScene/InGameManager/InGameBackground.cs
+++ Assets/01.Scripts/InGameScene/InGameManager/InGameBackground.cs
@@ -15,20 +15,11 @@ public class InGameBackground : MonoBehaviour
         return Color.HSVToRGB(Random.value, 1, 1);
     }
 
-    [ContextMenu("DD")]
     private void ChangeBackgroundColor(){
         topColor = GetRandomColor();
         bottomColor = GetRandomColor();
 
         lineRenderer.startColor = topColor;
         lineRenderer.endColor = bottomColor;
-    }
-
-    private void ChangeTopColor(){
-
-    }
-
-    private void ChangeBottomColor(){
-
     }
 }
