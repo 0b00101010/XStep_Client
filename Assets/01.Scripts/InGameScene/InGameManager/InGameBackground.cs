1861355a5273d8797fc3a1c4d3d791620dbea57a
Komorio <twilivez@gmail.com>
2020-05-06 02:40:03 +0900

    [ADD] InGame 배경 컨트롤러 추가

---
+++ Assets/01.Scripts/InGameScene/InGameManager/InGameBackground.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InGameBackground : MonoBehaviour
+{
+    private Color topColor;
+    private Color bottomColor;
+
+
+    [SerializeField]
+    private LineRenderer lineRenderer;
+
+    private Color GetRandomColor(){
+        return Color.HSVToRGB(Random.value, 1, 1);
+    }
+
+    [ContextMenu("DD")]
+    private void ChangeBackgroundColor(){
+        topColor = GetRandomColor();
+        bottomColor = GetRandomColor();
+
+        lineRenderer.startColor = topColor;
+        lineRenderer.endColor = bottomColor;
+    }
+
+    private void ChangeTopColor(){
+
+    }
+
+    private void ChangeBottomColor(){
+
+    }
+}
