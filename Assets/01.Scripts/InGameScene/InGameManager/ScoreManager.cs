4e7ded38f31d11e6586790aff4a97ff733a76a72
Komorio <twilivez@gmail.com>
2020-05-10 20:31:50 +0900

    [ADD] ScoreManager에 판정시 이벤트 시스템 추가
    00. JudgeImage 변경
    01. JudgeImage 사이즈 변경
    02. Score 추가

--- Assets/01.Scripts/InGameScene/InGameManager/ScoreManager.cs
+++ Assets/01.Scripts/InGameScene/InGameManager/ScoreManager.cs
@@ -21,20 +21,49 @@ public class ScoreManager : MonoBehaviour
 
     [Header("Resources")]
     [SerializeField]
-    private Sprite[] judgeImages;
+    private Sprite[] judgeSprites;
 
 
     [Header("Functions")]
     [SerializeField]
     private INTEvent numberConversionEvent;
 
-    public void AddScore(int score){
+    private Vector3 sizeUpVector;
+    
+    private Coroutine judgeImageSizeUpCoroutine;
+
+    private void Awake(){
+        sizeUpVector = Vector3.one * 0.7f;
+        sizeUpVector /= 30;
+    }
+
+    public void AddScore(int score, int judgeLevel){
         this.score += score;
         numberConversionEvent.Invoke(this.score);
+        judgeImage.sprite = judgeSprites[judgeSprites.Length - 1 - judgeLevel];
+        
+        if(judgeImageSizeUpCoroutine != null){
+            StopCoroutine(judgeImageSizeUpCoroutine);
+        }
+
+        judgeImageSizeUpCoroutine = StartCoroutine(JudgeImageSizeUpCoroutine());
+
     }
 
     public void RedutionHP(float value){
         hp -= value;
         hpImage.fillAmount = hp / defaultHP;
+    }
+
+    private IEnumerator JudgeImageSizeUpCoroutine(){
+        judgeImage.gameObject.SetActive(true);
+        judgeImage.gameObject.transform.localScale = Vector3.one;        
+        for(int i = 0; i < 10; i++){
+            judgeImage.gameObject.transform.localScale += sizeUpVector;
+            yield return YieldInstructionCache.WaitFrame;
+        }
+
+        yield return YieldInstructionCache.WaitingSeconds(1.0f);
+        judgeImage.gameObject.SetActive(false);
     }
 }
