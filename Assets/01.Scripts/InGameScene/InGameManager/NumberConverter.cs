df261ddbcb826a8ed7f2957edc348ff0711a8f5d
Komorio <twilivez@gmail.com>
2020-05-10 19:41:39 +0900

    [ADD] NumberConverter추가
    00. Image를 숫자에 맞게 변환시켜줌

---
+++ Assets/01.Scripts/InGameScene/InGameManager/NumberConverter.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NumberConverter : MonoBehaviour
+{
+
+    [Header("Objects")]
+    [SerializeField]
+    private Image[] imageObjects;
+
+    [Header("Resources")]
+    [SerializeField]
+    private Sprite[] spriteImages;
+
+    [ContextMenu("A")]
+    private void Test(){
+        Conversion(10);
+    }
+
+    public void Conversion(int value){
+        
+        bool notZero = false;
+
+        for(int i = 0; i < imageObjects.Length; i++){
+            int number = System.Convert.ToInt32(value.ToString("D" + imageObjects.Length)[i].ToString());
+            imageObjects[i].sprite = spriteImages[number];
+
+            if(number != 0 || i == imageObjects.Length - 1){
+                notZero = true;
+            }
+
+            if(notZero){
+                imageObjects[i].gameObject.SetActive(true);
+            }
+            else{
+                imageObjects[i].gameObject.SetActive(false);
+            }
+
+        }        
+    }
+}
