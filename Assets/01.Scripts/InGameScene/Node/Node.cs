d41d6c596e365d3ac47a7d94983cfb416b71928d
Komorio <twilivez@gmail.com>
2020-05-10 20:33:21 +0900

    [UPDATE] Base Node Script 수정
    00. 기본 스코어 계수 추가
    01. Interaction과 Failed Interaction virtual method 추가

--- Assets/01.Scripts/InGameScene/Node/Node.cs
+++ Assets/01.Scripts/InGameScene/Node/Node.cs
@@ -1,6 +1,24 @@
 using UnityEngine;
 
 public class Node : MonoBehaviour { 
+
+    [Header("Value")]
+    [SerializeField]
+    private int redutionHp;
+
+    [SerializeField]
+    private int basicScore;
+    
+    protected int BasicScore => basicScore;
+
     public virtual void Execute(Vector2 targetPosition) {}
-    public virtual void ObjectReset() {} 
+    public virtual void ObjectReset() {
+        gameObject.SetActive(false);
+    }
+
+    public virtual void Interaction() {}
+
+    public virtual void FailedInteraction() {
+        InGameManager.instance.scoreManager.RedutionHP(redutionHp);
+    }
 }
