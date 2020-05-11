e599dc2f02450703b73a32de4559564b8f11c1d2
Komorio <twilivez@gmail.com>
2020-05-11 17:53:27 +0900

    [ADD] NodeInteractionController 추가

---
+++ Assets/01.Scripts/InGameScene/InGameManager/NodeInteractionController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NodeInteractionController : MonoBehaviour
+{
+    private List<NormalNode> activeNormalNode = new List<NormalNode>();
+
+
+    public void AddActiveNormalNode(Node node){
+        activeNormalNode.Add(node as NormalNode);
+    }
+
+    public void RemoveActiveNormalNode(Node node){
+        activeNormalNode.Remove(node as NormalNode);
+    }   
+
+    public void NormalNodeInteraction(){
+        activeNormalNode[0].Interaction();
+    }
+}
