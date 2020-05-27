080e9dea0034f085437fd480e7f70c4a0a04940f
Komorio <twilivez@gmail.com>
2020-05-27 11:24:09 +0900

    [ADD] CustomYieldInstruction 추가
    00. WaitForTween

---
+++ Assets/01.Scripts/System/WaitForTween.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class WaitForTween : CustomYieldInstruction
+{
+    private Tween tween;
+
+    public override bool keepWaiting{
+        get{
+            return tween.IsPlaying();
+        }
+    }
+
+    public WaitForTween(Tween tween){
+        this.tween = tween;
+    }
+}
