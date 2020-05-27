afa6ead62ff562477dd513b4a9d31f09be0c89d0
Komorio <twilivez@gmail.com>
2020-05-27 09:24:20 +0900

    [UPDATE] UnityEvents class 통합

---
+++ Assets/01.Scripts/System/UnityEvents.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine.Events;
+using System;
+
+[Serializable]
+class VOIDEvent : UnityEvent{}
+
+[Serializable]
+public class NODEEvent : UnityEvent<Node, int> {}
+
+[Serializable]
+public class INTEvent : UnityEvent<int> {}
