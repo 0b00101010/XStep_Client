b3315cb63b1860368022af1638d7e717706378c7
Komorio <twilivez@gmail.com>
2020-05-27 21:16:14 +0900

    [UPDATE] NodeCreator 수정
    00. 자동 수동 메소드 분할
    01. Slide Node 추가

--- Assets/01.Scripts/InGameScene/InGameManager/NodeCreator.cs
+++ Assets/01.Scripts/InGameScene/InGameManager/NodeCreator.cs
@@ -5,25 +5,46 @@ using System.Linq;
 
 public class NodeCreator : MonoBehaviour
 {
+
+    [Header("Objects")]
     [SerializeField]
-    private GameObject nodeParentObject;
+    private GameObject normalNodeParentObject;
 
     [SerializeField]
-    private Transform[] targetTransforms;
+    private GameObject slideNodeParentObject;
+
+
+    [Header("Values")]
+    [SerializeField]
+    private Transform[] normalNodeTransforms;
+
+    [SerializeField]
+    private Transform[] slideNodeTransforms;
 
     private List<Node> normalNodes = new List<Node>();
-    private Vector2[] targetPositions;
+    private List<Node> slideNodes = new List<Node>();
+
+    private Vector2[] normalNodeTargetPositions;
+    private Vector2[] slideNodeTargetPositions;
 
     private void Awake(){
         Node[] tempNodes;
         
-        tempNodes = nodeParentObject.GetComponentsInChildren<NormalNode>(true); 
+        tempNodes = normalNodeParentObject.GetComponentsInChildren<NormalNode>(true); 
         normalNodes = tempNodes.ToList();
 
-        targetPositions = new Vector2[targetTransforms.Length];
+        tempNodes = slideNodeParentObject.GetComponentsInChildren<SlideNode>(true); 
+        slideNodes = tempNodes.ToList();
 
-        for(int i = 0; i < targetTransforms.Length; i++){
-            targetPositions[i] = targetTransforms[i].position; 
+        normalNodeTargetPositions = new Vector2[normalNodeTransforms.Length];
+        slideNodeTargetPositions = new Vector2[slideNodeTransforms.Length];
+
+        for(int i = 0; i < normalNodeTransforms.Length; i++){
+            normalNodeTargetPositions[i] = normalNodeTransforms[i].position; 
+        }
+
+        for(int i = 0; i < slideNodeTransforms.Length; i++){
+            slideNodeTargetPositions[i] = slideNodeTransforms[i].position;
         }
     }
 
@@ -39,19 +60,88 @@ public class NodeCreator : MonoBehaviour
     }
 
     private void NodeGenerate(){
-        Node node;
-        
         switch(Random.Range(0,3)){
             case 0:
             case 1:
+            SlideNodeGenerate();
+            break;
             case 2:
-            node = GetAvaliableNode(normalNodes);
-            node.Execute(targetPositions[Random.Range(0,targetPositions.Length)]);
+            NormalNodeGenerate();
             break;
             default:
             break;
         }
     }
+
+    private void NormalNodeGenerate(){
+        Node node = GetAvaliableNode(normalNodes);
+        node.Execute(normalNodeTargetPositions[Random.Range(0, normalNodeTargetPositions.Length)]);
+    }
+
+
+    public void NormalNodeGenerate(int index = 0){
+        Node node = GetAvaliableNode(normalNodes);
+        node.Execute(normalNodeTargetPositions[index]);
+    }
+
+    private void SlideNodeGenerate(){
+        int index = Random.Range(0, 8);
+
+        SlideNode node = GetAvaliableNode(slideNodes) as SlideNode;
+
+        Vector2 startPosition = Vector2.zero;
+        Vector2 targetPosition = Vector2.zero;
+        
+        SetSlideNodeVector(ref startPosition, ref targetPosition, index);
+        
+        node.Execute(startPosition, targetPosition);
+        node.SetObjectRotate(index);
+        node.SetSpriteFlip(index);
+    }
+    
+    // FIXME : 솔직히 조금 그렇지 않나
+    public void SlideNodeGenerate(int index = 0){
+        SlideNode node = GetAvaliableNode(slideNodes) as SlideNode;
+
+        Vector2 startPosition = Vector2.zero;
+        Vector2 targetPosition = Vector2.zero;
+        
+        SetSlideNodeVector(ref startPosition, ref targetPosition, index);
+        
+        node.SetObjectRotate(index);
+        node.SetSpriteFlip(index);
+        node.Execute(startPosition, targetPosition);
+    }
+
+    private void SetSlideNodeVector(ref Vector2 startPosition, ref Vector2 targetPosition, int index){  
+        switch(index){
+            case 0:
+            case 1:
+            startPosition = slideNodeTargetPositions[1];
+            targetPosition = slideNodeTargetPositions[0];
+            break;
+
+            case 2:
+            case 3:
+            startPosition = slideNodeTargetPositions[0];
+            targetPosition = slideNodeTargetPositions[1];
+            break;
+
+            case 4:
+            case 5:
+            startPosition = slideNodeTargetPositions[3];
+            targetPosition = slideNodeTargetPositions[2];
+            break;
+
+            case 6:
+            case 7:
+            startPosition = slideNodeTargetPositions[2];
+            targetPosition = slideNodeTargetPositions[3];
+            break;            
+        }
+
+    }
+
 
     private Node GetAvaliableNode(List<Node> nodes){
         Node returnNode = null;
