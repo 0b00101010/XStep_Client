00433dd84312a068860093e6cdb5526cb130ab42
Komorio <twilivez@gmail.com>
2020-06-11 10:15:56 +0900

    [ADD] ExtensionMethods class 추가

---
+++ Assets/01.Scripts/System/ExtensionMethods.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+public static class ExtensionMethods{
+    public static IEnumerator Start(this IEnumerator coroutine, MonoBehaviour behaviour){
+        behaviour.StartCoroutine(coroutine);
+        return coroutine;
+    }
+
+    public static void Stop(this IEnumerator coroutine, MonoBehaviour behaviour){
+        behaviour.StopCoroutine(coroutine);
+    }
+}
