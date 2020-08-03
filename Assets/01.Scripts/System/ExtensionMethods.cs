0d8da4bbc6d977fc64de404c149fca5ee825f555
Komorio <twilivez@gmail.com>
2020-08-03 10:47:34 +0900

    [UPDATE] ExtensionMethods 에 IndexOfMany 추가

--- Assets/01.Scripts/System/ExtensionMethods.cs
+++ Assets/01.Scripts/System/ExtensionMethods.cs
@@ -1,5 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
+
 public static class ExtensionMethods{
     public static IEnumerator Start(this IEnumerator coroutine, MonoBehaviour behaviour){
         behaviour.StartCoroutine(coroutine);
@@ -18,5 +20,17 @@ public static class ExtensionMethods{
 
     public static Vector2 Direction(this Vector2 start, Vector2 end){
         return (end - start).normalized;
+    }
+
+    public static int[] IndexOfMany(this string value, char findValue){
+        var findPosition = new List<int>();
+
+        for(int i = 0; i < value.Length; i++){
+            if(value[i].Equals(findValue)){
+                findPosition.Add(i);
+            }
+        }
+
+        return findPosition.ToArray(); 
     }
 }
