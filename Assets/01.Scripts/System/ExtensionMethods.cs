19637ad75aed7ba8c8fcfc73ca1098537941c014
Komorio <twilivez@gmail.com>
2020-08-18 10:37:46 +0900

    [UPDATE] Library 불러 옴

--- Assets/01.Scripts/System/ExtensionMethods.cs
+++ Assets/01.Scripts/System/ExtensionMethods.cs
@@ -3,21 +3,6 @@ using System.Collections;
 using System.Collections.Generic;
 
 public static class ExtensionMethods{
-    public static IEnumerator Start(this IEnumerator coroutine, MonoBehaviour behaviour){
-        behaviour.StartCoroutine(coroutine);
-        return coroutine;
-    }
-
-    public static void Stop(this IEnumerator coroutine, MonoBehaviour behaviour){
-        behaviour.StopCoroutine(coroutine);
-    }
-
-    public static void Log(this object value){
-        #if UNITY_EDITOR
-        Debug.Log(value.ToString());
-        #endif
-    }
-
     public static Vector2 Direction(this Vector2 start, Vector2 end){
         return (end - start).normalized;
     }
