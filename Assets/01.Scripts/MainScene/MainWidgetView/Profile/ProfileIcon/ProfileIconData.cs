ece3cb9053a16407f4ee262c2486aa7e1e6318b5
Komorio <twilivez@gmail.com>
2020-08-21 19:34:02 +0900

    [ADD] Profile Icon 관리할 Scriptable Object 및 Handler 추가

---
+++ Assets/01.Scripts/MainScene/MainWidgetView/Profile/ProfileIcon/ProfileIconData.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class IconData{
+    [SerializeField]
+    private Sprite _icon;
+    public Sprite icon => _icon;
+
+    [SerializeField]
+    private bool _isUnlock;
+    public bool isUnlock => _isUnlock;
+
+    public void Unlock(){
+        _isUnlock = true;
+    }
+}
+
+[CreateAssetMenu(fileName = "ProfileIconData", menuName = "Scriptable Object/ProfileIconData", order = 0)]
+public class ProfileIconData : ScriptableObject {
+    [SerializeField]
+    private List<IconData> iconDatas = new List<IconData>();
+    public int iconCount => iconDatas.Count;
+    
+    [SerializeField]
+    private IconData lockedIcon;
+
+    public IconData GetIcon(int index){
+        return iconDatas[index].isUnlock ? iconDatas[index] : lockedIcon;
+    }
+
+    public void UnlockIcon(int index){
+        iconDatas[index].Unlock();
+    }
+}
---
+++ Assets/01.Scripts/MainScene/MainWidgetView/Profile/ProfileIcon/ProfileIconHandler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProfileIconHandler : MonoBehaviour
+{
+    private ProfileIconData _iconData;
+    public ProfileIconData iconData => _iconData;
+
+    private void Awake(){
+        _iconData = Resources.Load<ProfileIconData>("IconData");
+    }
+
+    
+
+}
