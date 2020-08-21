517ab18620194ff89698f1ac04abbde55f83c834
Komorio <twilivez@gmail.com>
2020-08-21 13:31:30 +0900

    [ADD] Player Setting 읽어와서 View에 Setting 해주는 스크립트 추가

---
+++ Assets/01.Scripts/MainScene/MainWidgetView/Profile/ProfileView/ProfileViewController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class ProfileViewController : MonoBehaviour
+{
+    [Header("Setting Object")]
+    [SerializeField]
+    private Image userProfileImage;
+    
+    [SerializeField]
+    private TextMeshProUGUI userName;
+
+    [SerializeField]
+    private TextMeshProUGUI userTitle;
+
+    [Space(10)]
+    [SerializeField]
+    private TextMeshProUGUI levelText;
+
+    [SerializeField]
+    private Image expBar;
+
+    [SerializeField]
+    private TextMeshProUGUI expText;
+    
+    [Space(10)]
+    [SerializeField]
+    private TextMeshProUGUI totalScore;
+
+    [SerializeField]
+    private TextMeshProUGUI highClearScore;
+
+    [SerializeField]
+    private TextMeshProUGUI perfectPlay;
+
+    [SerializeField]
+    private TextMeshProUGUI challengeClear;
+
+    [SerializeField]
+    private TextMeshProUGUI freeStyleClear;
+
+    private void Start(){
+        var setting = GameManager.instance.PlayerSetting;
+    
+        userProfileImage.sprite = setting.profileSprite ?? userProfileImage.sprite;
+        userName.text = setting.userName;
+        userTitle.text = setting.title.ToString();
+    
+        levelText.text = $"Lv. {setting.currentLevel.ToString()}";
+        expBar.fillAmount = ((float)setting.currentExp / (float)setting.levelUpExp);
+        expText.text = $"{setting.currentExp} / {setting.levelUpExp}";
+
+        totalScore.text = setting.totalScore.ToString("D12");
+        highClearScore.text = setting.highClearScore.ToString("D2");
+
+        perfectPlay.text = setting.perfectPlay.ToString("D2");
+        challengeClear.text = setting.challengeClear.ToString("D2");
+        freeStyleClear.text = setting.freeStyleClera.ToString("D2");
+    }
+    
+}
