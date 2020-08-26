7ca3cc42b5af3fa689d2243dc7de47660949ee40
Komorio <twilivez@gmail.com>
2020-08-26 11:39:28 +0900

    [ADD Title Setting view 추가

---
+++ Assets/01.Scripts/MainScene/MainWidgetView/Profile/TitleSetting/ProfileTitleSettingView.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+public class ProfileTitleSettingView : ProfileSettingView
+{
+    [SerializeField]
+    private TitleItem[] titleItems;
+    private List<TitleItem> activeItems = new List<TitleItem>();
+    private ProfileTitleData titleData;
+
+    [SerializeField]
+    private TextMeshProUGUI titleText;
+
+    [SerializeField]
+    private TextMeshProUGUI descriptionText;
+
+    [SerializeField]
+    private Transform topPosition;
+
+    private void Awake(){
+        titleData = Resources.Load<ProfileTitleData>("Player Setting/ProfileTitleData");
+        titleItems = gameObject.GetComponentsInChildren<TitleItem>(true);
+
+        for(int i = 0; i < titleData.TitleResources.Length; i++){
+            titleItems[i].gameObject.SetActive(true);
+            titleItems[i].SettingTitle(titleData.TitleResources[i]);
+            titleItems[i].gameObject.transform.SetPositionY(topPosition.position.y - (i / 1.5f));
+            activeItems.Add(titleItems[i]);
+        }
+    }
+
+    private void Update(){
+        if(GameManager.instance.touchManager.IsSwipe){
+            if(GameManager.instance.touchManager.SwipeDirection.y > 0.8f){
+                "a".Log();
+                if(activeItems[activeItems.Count - 1].gameObject.transform.position.y < topPosition.position.y){
+                    MoveObject(Vector2.up);
+                    "b".Log();
+                }
+            }else if(GameManager.instance.touchManager.SwipeDirection.y < -0.8f){
+                if(activeItems[0].gameObject.transform.position.y > topPosition.position.y){
+                    MoveObject(Vector2.down);
+                }
+            }
+        }
+    }
+
+    private void MoveObject(Vector2 direction){
+        foreach(var item in activeItems){
+            item.transform.Translate(direction * 0.25f);
+        }
+    }
+
+    public void TitleChange(Title title){
+
+    }
+
+    public override void Execute(){
+        gameObject.SetActive(true);
+        titleText.text = GameManager.instance.PlayerSetting.title.title;
+        descriptionText.text = GameManager.instance.PlayerSetting.title.description;
+    }
+
+    public override void Exit(){
+        gameObject.SetActive(false);
+    }
+}
---
+++ Assets/01.Scripts/MainScene/MainWidgetView/Profile/TitleSetting/TitleItem.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using EventTools.Event;
+using TMPro;
+public class TitleItem : MainUIObject
+{
+    private Title item;
+
+    [SerializeField]
+    private TextMeshProUGUI titleName;
+
+    [SerializeField]
+    private Image unlockImage;
+
+    [SerializeField]
+    private Sprite[] unlockResources;
+    
+    [SerializeField]
+    private UniEvent<Title> executeEvent;
+
+    public override void Execute(){
+        executeEvent.Invoke(item);
+    }
+
+    public void SettingTitle(Title title){
+        item = title;
+        titleName.text = title.title;
+        unlockImage.sprite = title.isUnLock ? unlockResources[0] : unlockResources[1];
+    }
+}
