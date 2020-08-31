146879d6e23ae4581b24d403332846415abf7963
Komorio <twilivez@gmail.com>
2020-08-31 10:09:23 +0900

    [ADD] TrackUnlockView 추가

---
+++ Assets/01.Scripts/MainScene/MainWidgetView/TrackUnlocked/TrackUnlockView.cs
@@ -0,0 +1,50 @@
+using System.Security.Cryptography.X509Certificates;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+using TMPro;
+
+public class TrackUnlockView :MonoBehaviour {
+    private CanvasGroup canvasGroup;
+
+    [SerializeField] 
+    private Image backgroundImage;
+    
+    [SerializeField] 
+    private Image eyeCatch;
+
+    [SerializeField]
+    private TextMeshProUGUI songNameText;
+
+    [SerializeField]
+    private TextMeshProUGUI composerNameText;
+
+    [SerializeField]
+    private Text[] difficultyTexts;
+    
+    private void Awake() {
+        canvasGroup = gameObject.GetComponentInChildren<CanvasGroup>();
+    }
+    
+    public void Execute(SongItemInformation item) {
+        gameObject.SetActive(true);
+        
+        eyeCatch.sprite = item.EyeCatch;
+        for (int i = 0; i < item.Difficultys.Length; i++) {
+            difficultyTexts[i].text = item.Difficultys[i].ToString("D2");
+        }
+
+        songNameText.text = item.SongName;
+        composerNameText.text = item.ComposerName;
+        
+        GameManager.instance.widgetViewer.WidgetsOpen(backgroundImage, canvasGroup);
+        GameManager.instance.SomeUIInteraction = true;
+    }
+
+    public void Exit() {
+        GameManager.instance.widgetViewer.WidgetsClose(backgroundImage, () => {
+            GameManager.instance.SomeUIInteraction = false;
+            gameObject.SetActive(false);
+        }, canvasGroup);
+    }
+}
