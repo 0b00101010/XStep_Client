de4a127fa319d5d13da9ac9a6e5b91f369c5aafd
Komorio <twilivez@gmail.com>
2020-09-11 00:46:27 +0900

    [UPDATE] Metronome 코드 정리

--- Assets/01.Scripts/InGameScene/InGameManager/Metronome.cs
+++ Assets/01.Scripts/InGameScene/InGameManager/Metronome.cs
@@ -1,27 +1,24 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Metronome : MonoBehaviour
-{
+public class Metronome : MonoBehaviour {
+    private readonly Dictionary<string, double> settingDictionary = new Dictionary<string, double>();
+    private readonly List<SongProcessAction> songProcessActions = new List<SongProcessAction>();
     private AudioSource audioSource;
-    private MapFile songData;
-    
-    private Dictionary<string, double> settingDictionary = new Dictionary<string, double>();
-    private List<SongProcessAction> songProcessActions = new List<SongProcessAction>();
-    
-    private int split;
     private double bpm;
-    
-    private double offset;
 
     private double currentSample;
-
-    private double oneBeatTime; 
     private double nextStep;
 
-    private void Start(){
+    private double offset;
+
+    private double oneBeatTime;
+    private MapFile songData;
+
+    private int split;
+
+    private void Start() {
         audioSource = gameObject.GetComponent<AudioSource>();
         songData = GameManager.instance.songData;
 
@@ -30,12 +27,12 @@ public class Metronome : MonoBehaviour
         ReadFile();
 
         bpm = settingDictionary["BPM"];
-        split = (int)settingDictionary["Split"];
+        split = (int) settingDictionary["Split"];
         offset = settingDictionary["Delay"] / 1000;
 
         var offsetForSample = offset * audioSource.clip.frequency;
-        
-        oneBeatTime = (60.0 / bpm) / split;
+
+        oneBeatTime = 60.0 / bpm / split;
         oneBeatTime *= audioSource.clip.frequency;
 
         nextStep = offsetForSample;
@@ -43,138 +40,142 @@ public class Metronome : MonoBehaviour
         audioSource.Play();
     }
 
-    private void Update(){
-        if(audioSource.timeSamples >= nextStep){
+    private void Update() {
+        if (audioSource.timeSamples >= nextStep) {
             NodeGenerate();
         }
     }
 
-    private void NodeGenerate(){
-        int beforePosition = 0;
-        if(songProcessActions[0].positionValue != -1){
-            do{
+    private void NodeGenerate() {
+        var beforePosition = 0;
+        if (songProcessActions[0].positionValue != -1) {
+            do {
                 songProcessActions[0].currentProgressAction();
                 beforePosition = songProcessActions[0].positionValue;
                 songProcessActions.RemoveAt(0);
-            }while(beforePosition != 0 && songProcessActions[0].positionValue.Equals(beforePosition));
+            } while (beforePosition != 0 && songProcessActions[0].positionValue.Equals(beforePosition));
 
             nextStep += oneBeatTime;
-        } else {
-            do{
+        }
+        else {
+            do {
                 songProcessActions[0].currentProgressAction();
                 songProcessActions.RemoveAt(0);
-            }while(songProcessActions[0].positionValue == -1);
+            } while (songProcessActions[0].positionValue == -1);
         }
     }
 
-    private void ReadFile(){
+    private void ReadFile() {
         var mapTexts = songData.maps[songData.currentSelectDifficulty].map.text.Split('\n');
         var position = 0;
 
-        for(int i = 0; i < mapTexts.Length; i++){
-            if (mapTexts[i].Equals("") || mapTexts[i].Equals(" ")) {
-                continue;
-            }
-            
-            if(mapTexts[i].StartsWith(":")){
+        for (var i = 0; i < mapTexts.Length; i++) {
+            if (mapTexts[i].Equals("") || mapTexts[i].Equals(" ")) continue;
+
+            if (mapTexts[i].StartsWith(":")) {
                 var settingKey = mapTexts[i].Split(':')[1].Split('=')[0];
                 var settingValue = double.Parse(mapTexts[i].Split('=')[1]);
-                
+
                 MapSystemSetting(settingKey, settingValue);
-            }
-            else if (mapTexts[i].StartsWith("@")) {
-                var mapText = mapTexts[i].Split('@')[1];
-                var settingValue = mapText.Split(',');
 
-                var topColorHex = settingValue[0];
-                var bottomColorHex = settingValue[1];
-                float duration = float.Parse(settingValue[2]);
-                
-                var newProcessAction = new SongProcessAction();
- 
-                newProcessAction.positionValue = -1;
-                newProcessAction.currentProgressAction = BackgroundControlAction(topColorHex, bottomColorHex, duration);
-                
-                songProcessActions.Add(newProcessAction);
+                continue;
             }
-            else{
+
+            // Normal Node & Long Node
+            var frontText = mapTexts[i].Substring(0, 4);
+
+            // Slide Node
+            var backText = mapTexts[i].Substring(4, 4);
+
+            var normalNodePositions = frontText.IndexOfMany('X');
+            var longStartNodePositions = frontText.IndexOfMany('M');
+            var longEndNodePositions = frontText.IndexOfMany('W');
+
+            var leftSlideNodePositions = backText.IndexOfMany('X');
+            var rightSlideNodePositions = backText.IndexOfMany('M');
+
+            var nodeGenerateCount = normalNodePositions.Length +
+                                    longStartNodePositions.Length +
+                                    longEndNodePositions.Length +
+                                    leftSlideNodePositions.Length +
+                                    rightSlideNodePositions.Length;
+
+            if (nodeGenerateCount > 0) {
                 // Normal Node & Long Node
-                var frontText = mapTexts[i].Substring(0, 4);
-                
-                // Slide Node
-                var backText = mapTexts[i].Substring(4,4); 
+                void frontNodeGenerateFunction(int[] nodePositions, Func<int, Action> nodeGenerateAction) {
+                    for (var j = 0; j < nodePositions.Length; j++) {
+                        var newProcessAction = new SongProcessAction();
 
-                int[] normalNodePositions = frontText.IndexOfMany('X');
-                int[] longStartNodePositions = frontText.IndexOfMany('M');
-                int[] longEndNodePositions = frontText.IndexOfMany('W');
-                
-                int[] leftSlideNodePositions = backText.IndexOfMany('X');
-                int[] rightSlideNodePositions = backText.IndexOfMany('M');
-
-                int nodeGenerateCount = normalNodePositions.Length +
-                longStartNodePositions.Length + 
-                longEndNodePositions.Length + 
-                leftSlideNodePositions.Length + 
-                rightSlideNodePositions.Length;
-
-                if(nodeGenerateCount > 0){
-                    // Normal Node & Long Node
-                    void frontNodeGenerateFunction(int[] nodePositions, Func<int, Action> nodeGenerateAction){
-                        for(int j = 0; j < nodePositions.Length; j++){
-                            var newProcessAction = new SongProcessAction();
-
-                            newProcessAction.currentProgressAction 
+                        newProcessAction.currentProgressAction
                             = nodeGenerateAction(nodePositions[j]);
 
-                            newProcessAction.positionValue = position;
-
-                            songProcessActions.Add(newProcessAction);
-                        }
-                    }  
-
-                    void backNodeGenerateFunction(int index, int[] nodePositions){
-                        for(int j = 0; j < nodePositions.Length; j++){
-                            var newProcessAction = new SongProcessAction();
-
-                            int currentItemPosition = nodePositions[j];
-                            currentItemPosition = index.Equals(0) ? currentItemPosition : currentItemPosition + 1;
-
-                            newProcessAction.currentProgressAction 
-                            = SlideNodeGenerateAction(currentItemPosition);
-
-                            newProcessAction.positionValue = position;
-
-                            songProcessActions.Add(newProcessAction);
-                        }
+                        songProcessActions.Add(newProcessAction);
                     }
-
-                    frontNodeGenerateFunction(normalNodePositions, NormalNodeGenerateAction);
-                    frontNodeGenerateFunction(longStartNodePositions, LongNodeGenerateAction);
-                    frontNodeGenerateFunction(longEndNodePositions, LongNodeEndAction);
-
-                    backNodeGenerateFunction(0, leftSlideNodePositions);
-                    backNodeGenerateFunction(1, rightSlideNodePositions);
-                } else {
-                    var voidProcessAction = new SongProcessAction();
-                    
-                    voidProcessAction.currentProgressAction = () => {};
-                    voidProcessAction.positionValue = 0;
-                    
-                    songProcessActions.Add(voidProcessAction);
                 }
 
-                position++;
-            }
-        }
+                void backNodeGenerateFunction(int index, int[] nodePositions) {
+                    for (var j = 0; j < nodePositions.Length; j++) {
+                        var newProcessAction = new SongProcessAction();
 
+                        var currentItemPosition = nodePositions[j];
+                        currentItemPosition = index.Equals(0) ? currentItemPosition : currentItemPosition + 1;
+
+                        newProcessAction.currentProgressAction
+                            = SlideNodeGenerateAction(currentItemPosition);
+
+                        newProcessAction.positionValue = position;
+
+                        songProcessActions.Add(newProcessAction);
+                    }
+                }
+
+                frontNodeGenerateFunction(normalNodePositions, NormalNodeGenerateAction);
+                frontNodeGenerateFunction(longStartNodePositions, LongNodeGenerateAction);
+                frontNodeGenerateFunction(longEndNodePositions, LongNodeEndAction);
+
+                backNodeGenerateFunction(0, leftSlideNodePositions);
+                backNodeGenerateFunction(1, rightSlideNodePositions);
+            }
+            else {
+                var voidProcessAction = new SongProcessAction();
+
+                voidProcessAction.currentProgressAction = () => {
+                };
+                voidProcessAction.positionValue = 0;
+
+                songProcessActions.Add(voidProcessAction);
+            }
+
+            position++;
+
+
+            // if (mapTexts[i].Contains("@")) {
+            //     var mapText = mapTexts[i].Split('@')[1];
+            //     var settingValue = mapText.Split(',');
+            //
+            //     var topColorHex = settingValue[0];
+            //     var bottomColorHex = settingValue[1];
+            //     float duration = float.Parse(settingValue[2]);
+            //     
+            //     var newProcessAction = new SongProcessAction();
+            //
+            //     newProcessAction.positionValue = -1;
+            //     newProcessAction.currentProgressAction = BackgroundControlAction(topColorHex, bottomColorHex, duration);
+            //     
+            //     songProcessActions.Add(newProcessAction);
+            //
+            //     position++;
+            // }
+        }
     }
 
-    private void MapSystemSetting(string key, double value){
-        if(!settingDictionary.ContainsKey(key)){
+    private void MapSystemSetting(string key, double value) {
+        if (!settingDictionary.ContainsKey(key)) {
             settingDictionary.Add(key, value);
-        } else {
+        }
+        else {
             var newProcessAction = new SongProcessAction();
-            
+
             newProcessAction.currentProgressAction = MakeSystemAction(key, value);
             newProcessAction.positionValue = -1;
 
@@ -186,37 +187,39 @@ public class Metronome : MonoBehaviour
         return () => {
             Color hexToColorTop;
             Color hexToColorBottom;
-            
+
             ColorUtility.TryParseHtmlString(topColor, out hexToColorTop);
             ColorUtility.TryParseHtmlString(topColor, out hexToColorBottom);
-            
+
             InGameManager.instance.ChangeBackgroundColor(hexToColorTop, hexToColorBottom, duration);
         };
     }
 
-    private Action MakeSystemAction(string key, double value){
-        return () => { settingDictionary[key] = value; };
+    private Action MakeSystemAction(string key, double value) {
+        return () => {
+            settingDictionary[key] = value;
+        };
     }
 
-    private Action NormalNodeGenerateAction(int position){
+    private Action NormalNodeGenerateAction(int position) {
         return () => {
             InGameManager.instance.nodeCreator.NormalNodeGenerate(position);
         };
     }
 
-    private Action LongNodeGenerateAction(int position){
+    private Action LongNodeGenerateAction(int position) {
         return () => {
             InGameManager.instance.nodeCreator.LongNodeGenerate(position);
         };
     }
 
-    private Action LongNodeEndAction(int position){
+    private Action LongNodeEndAction(int position) {
         return () => {
             InGameManager.instance.nodeCreator.LongNodeStop(position);
         };
     }
 
-    private Action SlideNodeGenerateAction(int position){
+    private Action SlideNodeGenerateAction(int position) {
         return () => {
             InGameManager.instance.nodeCreator.SlideNodeGenerate(position);
         };
