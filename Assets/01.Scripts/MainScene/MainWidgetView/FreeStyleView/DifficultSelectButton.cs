a8d66337deb409b537d6cdd06c51e185af39b5e2
Komorio <twilivez@gmail.com>
2020-09-11 18:35:25 +0900

    [UPDATE] 곡 난이도 선택시 확인 할 수 있게 변경

--- Assets/01.Scripts/MainScene/MainWidgetView/FreeStyleView/DifficultSelectButton.cs
+++ Assets/01.Scripts/MainScene/MainWidgetView/FreeStyleView/DifficultSelectButton.cs
@@ -1,13 +1,32 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DifficultSelectButton : MainUIObject {
     [SerializeField]
     private int difficulty;
     
+    private Image backgroundImage;
+    public Image BackgroundImage => backgroundImage;
+
+    private static DifficultSelectButton _selectDifficultyButton;
+    public static DifficultSelectButton SelectDifficultyButton {
+        get => _selectDifficultyButton;
+        set => _selectDifficultyButton = value;
+    }
+
+    private void Awake() {
+        backgroundImage = gameObject.transform.GetChild(1).GetComponent<Image>();
+    }
+    
     public override void Execute() {
         GameManager.instance.songData.currentSelectDifficulty = difficulty;
+        
+        DifficultSelectButton.SelectDifficultyButton?.BackgroundImage.gameObject.SetActive(false);
+        DifficultSelectButton.SelectDifficultyButton = this;
+        
+        backgroundImage.gameObject.SetActive(true);
     }
     
 }
