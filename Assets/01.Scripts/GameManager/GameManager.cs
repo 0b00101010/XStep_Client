cb566ee5b26916e44b07d3201a31998c4602c04f
Komorio <twilivez@gmail.com>
2020-09-18 03:53:40 +0900

    [ADD] Connector 들 Controller 추가

--- Assets/01.Scripts/GameManager/GameManager.cs
+++ Assets/01.Scripts/GameManager/GameManager.cs
@@ -46,8 +46,12 @@ public class GameManager : DontDestroySingleton<GameManager>
     private bool someUIInteraction;
     public bool SomeUIInteraction {get => someUIInteraction; set => someUIInteraction = value;}
 
-    private ServerConnector serverConnector;
-    public ServerConnector ServerConnector => serverConnector;
+    private ServerConnectorController serverConnector;
+    public ServerConnectorController ServerConnector => serverConnector;
+
+    private string serverUrl;
+
+    public string ServerUrl => $"http://{serverUrl}";
 
     private void Awake(){
         if (instance != this && instance != null) {
@@ -56,15 +60,11 @@ public class GameManager : DontDestroySingleton<GameManager>
         
         touchManager = gameObject.GetComponent<TouchManager>();
         widgetViewer = gameObject.GetComponent<WidgetViewer>();
-        serverConnector = gameObject.GetComponent<ServerConnector>();
+        serverConnector = gameObject.GetComponent<ServerConnectorController>();
+
+        serverUrl = serverConnector.ServerUrl;
         
         playerSetting = Resources.Load<PlayerSetting>("Player Setting/PlayerSetting");
         playerSetting.AchieveRequireData.Initialize();
-    }
-    
-    [Button("Login")]
-    public void Login() {
-        var userData = serverConnector.Login("komorio", "0000");
-        userData?.Log();
     }
 }
---
+++ Assets/01.Scripts/System/Server/ServerConnectorController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Collections;
+using System.Collections.Generic;
+using NaughtyAttributes;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ServerConnectorController : MonoBehaviour {
+    [SerializeField]
+    private string serverUrl;
+    public string ServerUrl => serverUrl;
+
+    private LoginConnector loginConnector;
+
+    private void Awake() {
+        loginConnector = gameObject.GetComponent<LoginConnector>();
+    }
+
+    public void Login(string id, string passwd, UserData userData, params Action[] callbacks) {
+        for (int i = 0; i < callbacks.Length; i++) {
+            loginConnector.Callbacks.Add(callbacks[i]);
+        }
+        
+        loginConnector.Callbacks.Add(() => {
+            userData = loginConnector.GetUserData();
+        });
+        
+        loginConnector.OnRequest(id, passwd);
+    }
+}
