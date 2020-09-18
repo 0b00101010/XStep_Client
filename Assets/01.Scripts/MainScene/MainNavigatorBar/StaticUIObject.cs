92a09fe280a498ff1372c9b0273336ffa64e7b33
Komorio <twilivez@gmail.com>
2020-09-19 05:01:35 +0900

    [UPDATE] MainScene 연출 추가

---
+++ Assets/01.Scripts/MainScene/MainNavigatorBar/StaticUIObject.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class StaticUIObject : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject targetPosition;
+
+    private Vector3 defaultPosition;
+
+    private void Awake() {
+        defaultPosition = gameObject.transform.position;
+    }
+
+    private void OnEnable() {
+        gameObject.transform.position = defaultPosition;
+        gameObject.transform.DOMoveY(targetPosition.transform.position.y, 0.5f);
+    }
+}
---
+++ Assets/01.Scripts/MainScene/MainWidgetView/WidgetView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class WidgetView : MonoBehaviour {
+    private CanvasGroup canvasGroup;
+
+    private void Awake() {
+        canvasGroup = gameObject.GetComponent<CanvasGroup>();
+    }
+
+    private void OnEnable() {
+        canvasGroup.alpha = 0;
+        canvasGroup.DOFade(1.0f, 0.5f);
+    }
+}
